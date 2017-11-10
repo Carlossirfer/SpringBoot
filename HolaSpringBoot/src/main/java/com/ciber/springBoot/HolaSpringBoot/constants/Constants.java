f388a5424d5df7aabe8f5f6e2e619b6e0e6c8dce
carlossirfer <carlossirfer@gmail.com>
2017-11-10 14:18:36 +0100

    Añado clase constants.java
    Añado paginas html de error y posts.

---
+++ HolaSpringBoot/src/main/java/com/ciber/springBoot/HolaSpringBoot/constants/Constants.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.ciber.springBoot.HolaSpringBoot.constants;
+
+/**
+ * @author ciber
+ *
+ */
+public class Constants {
+
+	// CONSTANTE CON LA URL DE LA API
+	public static final String URL_API_BOOKS = "http://private-114e-booksapi.apiary-mock.com/books/";
+
+	// CONSTANTE CON LA URL DE LA API PRUEBA
+	public static final String URL_API_PRUEBA = "https://jsonplaceholder.typicode.com/posts/";
+
+}
